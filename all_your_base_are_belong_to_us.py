2a3b2cb6ab02020bbe525abd8c09c34762a2acb8
Ronnie <ronniedipple@gmail.com>
2020-03-23 23:13:01 -0600

    Guided project complete

---
+++ all_your_base_are_belong_to_us.py
@@ -0,0 +1,66 @@
+
+
+# Memorize Two to the power of 0 all the way to two to the power of ten, base 10
+
+# 2**0 = 1, 2**1=2 all the way to 10
+
+
+#Learn base 16 aka hex or hexidecimal
+# 2** 0 = 1
+# 2** 1 = 2
+# 2** 2 = 4
+# 2** 3 = 8
+# 2** 4 = 16
+# 2** 5 = 32
+# 2** 6 = 64
+# 2** 7 = 128
+# 2** 8 = 256
+# 2** 9 = 512
+# 2** 10 = 1024
+
+
+# 0b10100110 count from right to left
+
+# 0 = 0 these columns correspond exactly to the rows above
+# 1 = 2
+# 1 = 4
+# 0 = 0
+# 0 = 0
+# 1 = 32
+# 0 = 0
+# 1 = 128
+# 0 = 0
+
+#166
+
+# 123
+#
+# 10 ** 0 = 1
+# 10 ** 1 = 10
+# 10 ** 2 = 100
+
+# 0b 0011 0101 = 53
+
+# Hex
+
+#Version of 0b 0011 0101 = 53 below in hex
+
+0x35
+
+# Another one
+# 0b 1100 1011 = 0xCB = 203
+
+
+# Another one
+# 0b11111111 = 0xFF = 255
+
+
+# 0b 0011 0101 = 53 <- as a whole minus the 53 is called a byte and 0101 would be a nybble as would 0011
+# highest number in a nybble is 0b1111 which is 1 + 2 + 4 + 8 = 15
+
+# 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F 16 total values represented in hexidecimal
+
+
+
+
+
---
+++ ls8/simple_machine.py
@@ -0,0 +1,70 @@
+#Ls8 will use binary 1, 0s but this implements base 10
+import sys
+
+PRINT_BEEJ = 1
+HALT = 2
+PRINT_NUM = 3
+SAVE = 4 #SAVE a value to a register
+PRINT_REGISTER = 5 # Print a value from a register
+ADD = 6 # regA += regB This add will work the same as the ls 8
+
+# This represents RAM
+memory = [
+    PRINT_BEEJ,#Op code
+    SAVE,
+    65,
+    2,
+    SAVE,
+    20,
+    3,
+    ADD,
+    2,
+    3,
+    PRINT_REGISTER,
+    2,
+    HALT
+]
+
+register = [0] * 8 # Registers are ulter fast but limited in size and quantity can only store 1 what is called a WORD
+
+# program_counter represents a register that points to a location in memory
+program_counter = 0
+flag_running = True
+
+# While is the processor
+while flag_running:
+    command = memory[program_counter]
+
+    if command == PRINT_BEEJ:
+        print("Beej!")
+        program_counter += 1
+
+    elif command == HALT:
+        flag_running = False
+        program_counter += 1
+
+    elif command == PRINT_NUM:
+        num = memory[program_counter + 1]
+        print(num)
+        program_counter += 2
+    elif command == SAVE:
+        num = memory[program_counter + 1]
+        reg = memory[program_counter + 2]
+        register[reg] = num
+        program_counter += 3
+    elif command == PRINT_REGISTER:
+        reg = memory[program_counter + 1]
+        print(register[reg])
+        program_counter += 2
+    elif command == ADD:
+        reg_a = memory[program_counter + 1]
+        reg_b = memory[program_counter + 2]
+        register[reg_a] += register[reg_b]
+        program_counter += 3
+
+
+    else:
+        print(f"Unknown instruction: {command}")
+        sys.exit(1)
+
+
