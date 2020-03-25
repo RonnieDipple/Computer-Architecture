98f9cba87668c8d7012230cdd1726aa2b7a08778
Ronnie <ronniedipple@gmail.com>
2020-03-24 22:25:47 -0500

    MVP

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -7,5 +7,5 @@ from cpu import *
 # program = sys.argv[1]
 cpu = CPU()
 
-cpu.load("examples/mult.ls8") #removedprogram for testing
+cpu.load("examples/mult.ls8") #removed program for testing
 cpu.run()
